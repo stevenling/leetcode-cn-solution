491708222e94db2a1fc5ab6a9919455af7c9b2bd
yunhu <703019049@qq.com>
2022-05-29 16:47:08 +0800

    docs: add 009

---
+++ src/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hashMap;
+        vector<int> res;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (hashMap.count(nums[i]) == 0) {
+                // 不存在这个元素
+                hashMap[nums[i]] = i; // 键是 vector 的值，值是下标 
+            }
+            if (hashMap.count(nums[i]) == 1) {
+                // 存在这个元素
+                if(hashMap.count(nums[i] - target) == 1) {
+                    res.push_back(i);
+                    res.push_back(hashMap[nums[i] - target]);
+                    return res;
+                }
+            }
+        }
+    }
+};
